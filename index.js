fd8212f9c1fcd3ae2a812fe93abaa056497f59a7
kh4n998 <ka.0709fe@gmail.com>
2023-12-29 07:15:09 +0700

    fix prefer motion to reduce

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const observer = new IntersectionObserver(entries => {
-    console.log(entries);
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("visible", "opacity-1", "blur-none", "translate-x-0");
-            entry.target.classList.remove("invisible", "opacity-0", "blur-sm", "-translate-x-full");
-        } 
-        // else {
-        //     entry.target.classList.add("invisible", "opacity-0", "blur-sm", "-translate-x-full");
-        //     entry.target.classList.remove("visible", "opacity-1", "blur-none", "translate-x-0");
-        // }
-    })
-})
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver(entries => {
+        console.log(entries);
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible", "opacity-1", "blur-none", "translate-x-0");
+                entry.target.classList.remove("invisible", "opacity-0", "blur-sm", "-translate-x-full");
+            } 
+            // else {
+            //     entry.target.classList.add("invisible", "opacity-0", "blur-sm", "-translate-x-full");
+            //     entry.target.classList.remove("visible", "opacity-1", "blur-none", "translate-x-0");
+            // }
+        })
+    }, {threshold: 0.5})
+    
+    document.querySelectorAll("section[data-section]").forEach(section => observer.observe(section))
 
-document.querySelectorAll("section[data-section]").forEach(section => observer.observe(section))
+})
