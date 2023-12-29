659a35d808a3fcb4a441fed3b6f215d8e9d3a3db
kh4n998 <ka.0709fe@gmail.com>
2023-12-29 14:20:14 +0700

    fix animation

--- index.js
+++ index.js
@@ -2,17 +2,37 @@ document.addEventListener("DOMContentLoaded", () => {
     const observer = new IntersectionObserver(entries => {
         console.log(entries);
         entries.forEach((entry) => {
+            
             if (entry.isIntersecting) {
-                entry.target.classList.add("visible", "opacity-1", "blur-none", "translate-x-0");
-                entry.target.classList.remove("invisible", "opacity-0", "blur-sm", "-translate-x-full");
+                if (entry.target.id === "info_hero") {
+                    entry.target.classList.add("visible", "opacity-1", "blur-none", "translate-x-0");
+                    entry.target.classList.remove("invisible", "opacity-0", "blur-sm", "-translate-x-full");
+                } else if (entry.target.id === "img_hero") {
+                    entry.target.classList.add("visible", "opacity-1", "blur-none", "scale-1");
+                    entry.target.classList.remove("invisible", "opacity-0", "blur-sm", "scale-0");
+                } else {
+                    entry.target.classList.remove("invisible", "opacity-0", "blur-sm", "-translate-x-full");
+                    entry.target.classList.add("visible", "opacity-1", "blur-none", "translate-x-0");
+                    const h2 = entry.target.querySelector("h2");
+                    h2.classList.add("after:w-full");
+                    h2.classList.remove("after:w-0");
+                }
             } 
             // else {
             //     entry.target.classList.add("invisible", "opacity-0", "blur-sm", "-translate-x-full");
             //     entry.target.classList.remove("visible", "opacity-1", "blur-none", "translate-x-0");
             // }
         })
-    }, {threshold: 0.5})
-    
+    })
     document.querySelectorAll("section[data-section]").forEach(section => observer.observe(section))
+    
+    //gen hero section
+    const homeSection = document.querySelector("#home");
+    const imgHero = homeSection.querySelector("#img_hero");
+    const infoHero = homeSection.querySelector("#info_hero");
+    observer.observe(imgHero)
+    observer.observe(infoHero)
+
 
 })
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,24 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-        'brand': ['"Yellowtail"', 'cursive'],
-        'mono': ['"Space Mono"', 'monospace'],
-        'code': ['"Source Code Pro"', 'monospace']
+      'brand': ['"Yellowtail"', 'cursive'],
+      'mono': ['"Space Mono"', 'monospace'],
+      'code': ['"Source Code Pro"', 'monospace']
     },
     extend: {
-        colors: {
-            "primary": "#00cf00",
-            "body": "#263238",
-            "neon": "#8ce436"
+      colors: {
+        "primary": "#00cf00",
+        "body": "#263238",
+        "neon": "#8ce436"
+      },
+      extend: {
+        transitionProperty: {
+          'width': 'width'
+        },
+        transitionDuration: {
+          '2000': '2000ms',
         }
+      }
     }
   },
   plugins: [],
